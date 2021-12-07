b8d949911e6c926b6e54e40d49e3bae704bd4b5a
jjstephen1 <john.jaya2001@gmail.com>
2021-12-07 19:21:29 +0530

    commit

--- addlast.java
+++ addlast.java
@@ -4,53 +4,51 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class addlast {
-    
-    private static class Node
-    {
+
+    private static class Node {
         int data;
         Node next;
     }
 
-    private static class LinkedList
-    {
+    private static class LinkedList {
         Node head;
         Node tail;
         int size;
 
-        
-        void addLast(int data)
-        {
+        void addLast(int data) {
             Node temp = new Node();
             temp.data = data;
             temp.next = null;
-            if(head == null)
-            {
-                head = tail = temp;
-                size++;
-            }
-            else
-            {
-                tail.next = temp;
-                tail = temp;
-                size++;
-            }
-            
+            // if(head == null)
+            // {
+            // head = tail = temp;
+            // size++;
+            // }
+            // else
+            // {
+            // tail.next = temp;
+            // tail = temp;
+            // size++;
+            // }
+
+            tail.next = temp;
+            tail = temp;
+            size++;
+
         }
-        int size()
-        {
+
+        int size() {
             return size;
         }
-        void display()
-        {
-            for(Node temp = head;temp != null;temp = temp.next)
-            {
+
+        void display() {
+            for (Node temp = head; temp != null; temp = temp.next) {
                 System.out.println(temp.data + "\t");
             }
             System.out.println();
         }
+
     }
-    
-    
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -61,13 +59,9 @@ public class addlast {
             if (str.startsWith("addLast")) {
                 int val = Integer.parseInt(str.split(" ")[1]);
                 list.addLast(val);
-            }
-            else if(str.startsWith("size"))
-            {
+            } else if (str.startsWith("size")) {
                 System.out.println(list.size());
-            }
-            else if(str.startsWith("display"))
-            {
+            } else if (str.startsWith("display")) {
                 list.display();
             }
 
