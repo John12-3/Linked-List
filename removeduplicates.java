5f0dc1190b650f7c4fab51dfb4d3500df5401a8c
jjstephen1 <john.jaya2001@gmail.com>
2021-12-21 00:04:27 +0530

    commit

--- removeduplicates.java
+++ removeduplicates.java
@@ -1,5 +1,6 @@
 
 import java.io.*;
+import java.util.LinkedList;
 
 public class removeduplicates {
     public static class Node {
@@ -282,6 +283,21 @@ public class removeduplicates {
 
         public void removeDuplicates() {
             // write your code here
+            LinkedList res = new LinkedList();
+
+            while(this.size() > 0)
+            {
+                int val = this.getFirst();
+                this.removeFirst();
+                if(res.size == 0 || res.tail.data != val)
+                {
+                    res.addLast(val);
+                }
+            }
+
+            this.head = res.head;
+            this.tail = res.tail;
+            this.size = res.size;
         }
     }
 
