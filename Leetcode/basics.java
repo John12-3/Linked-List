1694ca9da46ffe7686b658a71dbfa095ea855306
jjstephen1 <john.jaya2001@gmail.com>
2021-12-21 19:29:22 +0530

    commit

---
+++ Leetcode/basics.java
@@ -0,0 +1,29 @@
+package Leetcode;
+
+public class basics {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
