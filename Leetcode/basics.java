6ac710e5a687ba80197e2a5786a0107f9f40971b
jjstephen1 <john.jaya2001@gmail.com>
2021-12-23 16:26:33 +0530

    commit

--- Leetcode/basics.java
+++ Leetcode/basics.java
@@ -68,9 +68,30 @@ public class basics {
 
     public void reorderList(ListNode head)
     {
-        if(head == null || head.next == null) return head;
+        if(head == null || head.next == null) return ;
 
+        ListNode midnode = middleNode(head); // middle node
+        ListNode nhead = midnode.next;  // the new head is the head of the 2nd List
+        midnode.next = null;
+
+        nhead = reverseList(nhead); // the 2nd list becomes reverse
+
+        ListNode c1 = head;
+        ListNode c2 = nhead;
         
+        while(c2 != null)
+        {
+            // backup
+            ListNode f1 = c1.next;
+            ListNode f2 = c2.next;
+
+            c1.next = c2;
+            c2.next = f1;
+            c1 = f1;
+            c2 = f2;
+        }
+        
+
     }
 
     public static void main(String[] args) {
